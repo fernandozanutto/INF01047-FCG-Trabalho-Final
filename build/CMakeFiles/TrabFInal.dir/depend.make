fa89ff2448b5927191b67af3eb10b3996905ee80
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-04-12 22:29:29 -0300

    Gitignore

--- build/CMakeFiles/TrabFInal.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TrabFinal.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ferna/Documents/GitHub/INF01047-FCG-Trabalho-Final
-# Build directory: C:/Users/ferna/Documents/GitHub/INF01047-FCG-Trabalho-Final/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
