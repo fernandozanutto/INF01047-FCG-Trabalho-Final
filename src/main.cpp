a9f60d203b39d622ba6a3b6360cba6e3c9f726bb
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-04-16 19:10:54 -0300

    Removendo build triangles da main

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,6 @@ void PopMatrix(glm::mat4& M);
 
 // Declaração de várias funções utilizadas em main().  Essas estão definidas
 // logo após a definição de main() neste arquivo.
-void BuildTrianglesAndAddToVirtualScene(Model*);                           // Constrói representação de um ObjModel como malha de triângulos para renderização
 void LoadShadersFromFiles();                                                  // Carrega os shaders de vértice e fragmento, criando um programa de GPU
 void LoadTextureImage(const char* filename);                                  // Função que carrega imagens de textura
 GLuint LoadShader_Vertex(const char* filename);                               // Carrega um vertex shader
@@ -210,15 +209,12 @@ int main(int argc, char* argv[]) {
 
     // Construímos a representação de objetos geométricos através de malhas de triângulos
     Model spheremodel("sphere");
-    BuildTrianglesAndAddToVirtualScene(&spheremodel);
-
     Model bunnymodel("bunny");
-    BuildTrianglesAndAddToVirtualScene(&bunnymodel);
-
     Model planemodel("plane");
-    BuildTrianglesAndAddToVirtualScene(&planemodel);
 
-    Renderer rendererr(windoww.width, windoww.height, g_VirtualScene);
+    Renderer rendererr(windoww.width, windoww.height, g_VirtualScene, &spheremodel, &bunnymodel, &planemodel);
+    
+    std::cout << spheremodel.vaoId.at(0) << std::endl;
 
     // Inicializamos o código para renderização de texto.
     TextRendering_Init();
@@ -386,104 +382,6 @@ void PopMatrix(glm::mat4& M) {
         M = g_MatrixStack.top();
         g_MatrixStack.pop();
     }
-}
-
-// Constrói triângulos para futura renderização a partir de um ObjModel.
-void BuildTrianglesAndAddToVirtualScene(Model* model) {
-    GLuint vertex_array_object_id;
-    glGenVertexArrays(1, &vertex_array_object_id);
-    glBindVertexArray(vertex_array_object_id);
-
-    std::vector<GLuint> indices;
-    std::vector<float> model_coefficients;
-    std::vector<float> normal_coefficients;
-    std::vector<float> texture_coefficients;
-
-    for (size_t shape = 0; shape < model->shapes.size(); ++shape) {
-        size_t first_index = indices.size();
-        size_t num_triangles = model->shapes[shape].mesh.num_face_vertices.size();
-
-        const float minval = std::numeric_limits<float>::min();
-        const float maxval = std::numeric_limits<float>::max();
-
-        glm::vec3 bbox_min = glm::vec3(maxval, maxval, maxval);
-        glm::vec3 bbox_max = glm::vec3(minval, minval, minval);
-
-        for (size_t triangle = 0; triangle < num_triangles; ++triangle) {
-            assert(model->shapes[shape].mesh.num_face_vertices[triangle] == 3);
-
-            for (size_t vertex = 0; vertex < 3; ++vertex) {
-                tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
-
-                indices.push_back(first_index + 3 * triangle + vertex);
-
-                const float vx = model->attrib.vertices[3 * idx.vertex_index + 0];
-                const float vy = model->attrib.vertices[3 * idx.vertex_index + 1];
-                const float vz = model->attrib.vertices[3 * idx.vertex_index + 2];
-                // printf("tri %d vert %d = (%.2f, %.2f, %.2f)\n", (int)triangle, (int)vertex, vx, vy, vz);
-                model_coefficients.push_back(vx);    // X
-                model_coefficients.push_back(vy);    // Y
-                model_coefficients.push_back(vz);    // Z
-                model_coefficients.push_back(1.0f);  // W
-
-                bbox_min.x = std::min(bbox_min.x, vx);
-                bbox_min.y = std::min(bbox_min.y, vy);
-                bbox_min.z = std::min(bbox_min.z, vz);
-                bbox_max.x = std::max(bbox_max.x, vx);
-                bbox_max.y = std::max(bbox_max.y, vy);
-                bbox_max.z = std::max(bbox_max.z, vz);
-
-                if (idx.normal_index != -1) {
-                    const float nx = model->attrib.normals[3 * idx.normal_index + 0];
-                    const float ny = model->attrib.normals[3 * idx.normal_index + 1];
-                    const float nz = model->attrib.normals[3 * idx.normal_index + 2];
-                    normal_coefficients.push_back(nx);    // X
-                    normal_coefficients.push_back(ny);    // Y
-                    normal_coefficients.push_back(nz);    // Z
-                    normal_coefficients.push_back(0.0f);  // W
-                }
-
-                if (idx.texcoord_index != -1) {
-                    const float u = model->attrib.texcoords[2 * idx.texcoord_index + 0];
-                    const float v = model->attrib.texcoords[2 * idx.texcoord_index + 1];
-                    texture_coefficients.push_back(u);
-                    texture_coefficients.push_back(v);
-                }
-            }
-        }
-
-        size_t last_index = indices.size() - 1;
-
-        SceneObject theobject;
-        theobject.name = model->shapes[shape].name;
-        theobject.first_index = first_index;                   // Primeiro índice
-        theobject.num_indices = last_index - first_index + 1;  // Número de indices
-        theobject.rendering_mode = GL_TRIANGLES;               // Índices correspondem ao tipo de rasterização GL_TRIANGLES.
-        theobject.vertex_array_object_id = vertex_array_object_id;
-
-        theobject.bbox_min = bbox_min;
-        theobject.bbox_max = bbox_max;
-
-        std::cout << model->shapes[shape].name << std::endl;
-        
-        g_VirtualScene[model->shapes[shape].name] = theobject;
-    }
-
-    VBO VBO_model_coefficients(model_coefficients.data(), model_coefficients.size() * sizeof(float), 0, 4);
-
-    if (!normal_coefficients.empty()) {
-        VBO VBO_normal_coefficients(normal_coefficients.data(), normal_coefficients.size() * sizeof(float), 1, 4);
-    }
-
-    if (!texture_coefficients.empty()) {
-        VBO VBO_texture_coefficients(texture_coefficients.data(), texture_coefficients.size() * sizeof(float), 2, 2);
-    }
-
-    IBO indexBuffer(indices.data(), indices.size() * sizeof(GLuint));
-
-    // "Desligamos" o VAO, evitando assim que operações posteriores venham a
-    // alterar o mesmo. Isso evita bugs.
-    glBindVertexArray(0);
 }
 
 // Carrega um Vertex Shader de um arquivo GLSL. Veja definição de LoadShader() abaixo.
