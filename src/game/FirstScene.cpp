dc547c69412f0ccbb4ca9dbbdadc34495db69096
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-04-20 08:46:27 -0300

    Ajustando ordem da FirstScene

--- src/game/FirstScene.cpp
+++ src/game/FirstScene.cpp
@@ -12,17 +12,18 @@ FirstScene::FirstScene() {
 
     GameObject sphere(new Model("sphere"));
     sphere.rotateX(0.2f)->rotateZ(0.6f)->translate(-1.0f, 0, 0)->setAngularVelocity(0, 0.3f, 0)->setVelocity(0, 0, 0);
-    this->gameObjects.push_back(sphere);
-
+    
     GameObject bunny(bunnyModel);
     bunny.translate(1,0,0)->setAngularVelocity(1.0f, 0.2f, 0);
-    this->gameObjects.push_back(bunny);
-
+    
     GameObject smallBunny(bunnyModel);
     smallBunny.scale(0.3f, 0.3f, 0.3f)->translate(0.0f, 2.0f, 0.0f);
-    this->gameObjects.push_back(smallBunny);
-
+    
     GameObject plane(new Model("plane"));
     plane.scale(2.0f, 1.0f, 2.0f)->translate(0, -1.1f, 0);
+    
+    this->gameObjects.push_back(sphere);
+    this->gameObjects.push_back(bunny);
     this->gameObjects.push_back(plane);
+    this->gameObjects.push_back(smallBunny);
 }
