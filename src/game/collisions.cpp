a36af7832e03e57ddc7aa96411a159f6530fe206
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-05-08 22:01:48 -0300

    Removendo cometarios

--- src/game/collisions.cpp
+++ src/game/collisions.cpp
@@ -5,17 +5,10 @@
 
 #define DELTA 0.004
 
-bool boundingBoxCollision(GameObject& first, GameObject& second) {
-    return false;
-}
-
 bool boundBoxPlaneCollision(BoundingBox& object, BoundingBox& plane) {
     bool xAxis = object.min.x <= plane.max.x && object.max.x >= plane.min.x;
     bool yAxis = object.min.y <= plane.max.y && object.max.y >= plane.min.y;
     bool zAxis = object.min.z <= plane.max.z && object.max.z >= plane.min.z;
-    //std::cout << "obj coords:   " << object.min.y << " " << object.max.y << " " << object.min.x << " " << object.max.x << " " << object.min.z << " " << object.max.z << std::endl;
-    //std::cout << "plane coords: " << plane.min.y << " " << plane.max.y << " " << plane.min.x << " " << plane.max.x << " " << plane.min.z << " " << plane.max.z << std::endl;
-    //std::cout << yAxis << " " << xAxis << " " << zAxis << std::endl;
     return yAxis && xAxis && zAxis;
 }
 
@@ -23,9 +16,6 @@ bool pointBoundingBoxCollision(glm::vec4 point, BoundingBox& bb) {
     bool xAxis = (point.x - bb.max.x) <= DELTA && (point.x - bb.min.x) >= DELTA;
     bool yAxis = (point.y - bb.max.y) <= DELTA && (point.y - bb.min.y) >= DELTA;
     bool zAxis = (point.z - bb.max.z) <= DELTA && (point.z - bb.min.z) >= DELTA;
-    //std::cout << "obj coords:   " << point.y << " " << point.x << " " << point.z << std::endl;
-    //std::cout << "bb coords: " << bb.min.y << " " << bb.max.y << " " << bb.min.x << " " << bb.max.x << " " << bb.min.z << " " << bb.max.z << std::endl;
-    //std::cout << yAxis << " " << xAxis << " " << zAxis << std::endl;
     return yAxis && xAxis && zAxis;
 }
 
@@ -33,9 +23,6 @@ bool pointPlaneCollision(glm::vec4 point, BoundingBox& bb) {
     bool yAxis = (point.y - (bb.max.y)) <= DELTA && (point.y - (bb.min.y)) >= -DELTA;
     bool xAxis = (point.x - (bb.max.x)) <= DELTA && (point.x - (bb.min.x)) >= -DELTA;
     bool zAxis = (point.z - (bb.max.z)) <= DELTA && (point.z - (bb.min.z)) >= -DELTA;
-    //std::cout << "obj coords:   " << point.y << " " << point.x << " " << point.z << std::endl;
-    //std::cout << "bb coords: " << bb.min.y << " " << bb.max.y << " " << bb.min.x << " " << bb.max.x << " " << bb.min.z << " " << bb.max.z << std::endl;
-    //std::cout << yAxis << " " << xAxis << " " << zAxis << std::endl;
     return yAxis && xAxis && zAxis;
 }
 
--- src/game/collisions.h
+++ src/game/collisions.h
@@ -4,11 +4,8 @@
 #include "../renderer/BoundingBox.h"
 
 
-bool boundingBoxCollision(GameObject& first, GameObject& second);
-
 bool boundBoxPlaneCollision(BoundingBox& object, BoundingBox& plane);
 bool boundBoxCollision(BoundingBox& object1, BoundingBox& object2);
-
 bool pointBoundingBoxCollision(glm::vec4 point, BoundingBox& bb);
 bool pointPlaneCollision(glm::vec4 point, BoundingBox& bb);
 
